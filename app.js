7780e7c7b2fcacd92a9deb37f06c933f490f32d5
nengzhizhi <nengzhizhi@gmail.com>
2016-04-20 22:37:36 +0800

    add orientation

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var express = require('express')
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, '../static/')));
+app.use(express.static(path.resolve(__dirname, './static/')));
 app.listen(8001);
